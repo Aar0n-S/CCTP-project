a13d89263344b7d2dd1379ebc5e84137c113a525
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2025-04-27 18:00:42 +0100

    fixed mobile inputs

--- Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
+++ Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -11,11 +11,13 @@ namespace StarterAssets
         public void VirtualMoveInput(Vector2 virtualMoveDirection)
         {
             starterAssetsInputs.MoveInput(virtualMoveDirection);
+            
         }
 
         public void VirtualLookInput(Vector2 virtualLookDirection)
         {
             starterAssetsInputs.LookInput(virtualLookDirection);
+           
         }
 
         public void VirtualJumpInput(bool virtualJumpState)
