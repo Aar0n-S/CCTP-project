bae8538162e44aceecb77afb92bcf35bbe8b5383
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2025-04-28 10:28:50 +0100

    main menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   //load the scene 
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //quit the game
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("palyer has quit");
+    }
+
+}
